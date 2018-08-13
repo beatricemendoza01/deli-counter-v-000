e0110d5f9fcca3373153ebe24408e99667c33d80
Beatrice Mendoza <mendoza@nyu.edu>
2018-08-13 16:58:19 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,1 +1,12 @@
-# Write your code here.
+katz_deli = []
+
+def line
+  if katz_deli.length == 0
+    "The line is currently empty."
+  else
+    katz_deli.each_with_index do |person, index|
+      katz_deli[index].insert(0, "#{index + 1}. ")
+    end
+    "The line is currently: #{katz_deli.join(" ")}"
+  end
+end
